c35f7ef9521177bb18cd0bb00081951a70585039
dblaning <dblaning@alum.mit.edu>
2021-07-23 11:42:02 -0700

    Update Posts.js

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -11,6 +11,10 @@ export default class extends AbstractView {
             <h1>Posts</h1>
             <p>You are viewing the posts by Dave!</p>
             <p><a href="https://1drv.ms/p/s!AjQFDnjd2WJCgaRrKpVWRIkEt642Gw?e=zsXc05" target="_blank">Gavel May 17, 1971</a></p>
+            <p><a href="../clickerpage42.pdf" target="_blank">Gavel-date?-ONE_up</a></p>
+            <p><a href="../../clickerpage42.pdf" target="_blank">Gavel-date?-TWO_up</a></p>
+            <p><a href="../../../clickerpage42.pdf" target="_blank">Gavel-date?-THREE_up</a></p>
+                  
         `;
     }
 }
