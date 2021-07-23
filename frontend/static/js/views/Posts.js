703aaca3558f625281370114b2ed30b3c89d63f8
dblaning <dblaning@alum.mit.edu>
2021-07-23 12:06:15 -0700

    Update Posts.js

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -10,13 +10,76 @@ export default class extends AbstractView {
         return `
             <h1>Posts</h1>
             <p>You are viewing the posts by Dave!</p>
-            <p><a href="https://1drv.ms/p/s!AjQFDnjd2WJCgaRrKpVWRIkEt642Gw?e=zsXc05" target="_blank">Gavel May 17, 1971</a></p>
-            <p><a href="../clickerpage42.pdf" target="_blank">Gavel-date?-ONE_up</a></p>
-            <p><a href="../../clickerpage42.pdf" target="_blank">Gavel-date?-TWO_up</a></p>
-            <p><a href="../../../clickerpage42.pdf" target="_blank">Gavel-date?-THREE_up</a></p>
+            <p><a href="/static/clickerpage24.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage25.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage26.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage27.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage28.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage29.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage30.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage31.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage32.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage33.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage34.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage35.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage36.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage37.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage38.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage39.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage40.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage41.pdf" target="_blank">Gavel-date?-static-one</a></p>
             <p><a href="/static/clickerpage42.pdf" target="_blank">Gavel-date?-static-one</a></p>
-                  
-                  
-        `;
+            <p><a href="/static/clickerpage43.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage44.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage45.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage46.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage47.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage48.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage49.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage50.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage51.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage52.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage53.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage54.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage55.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage56.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage57.pdf" target="_blank">Gavel-date?-static-one</a></p>              
+            <p><a href="/static/clickerpage58.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage59.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage60.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage61.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage62.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage63.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage64.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage65.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage66.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage67.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage68.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage69.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage70.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage71.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage72.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage73.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage74.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage75.pdf" target="_blank">Gavel-date?-static-one</a></p>              
+            <p><a href="/static/clickerpage76.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage77.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage78.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage79.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage80.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage81.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage82.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage83.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage84.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage85.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage86.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage87.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage88.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage89.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage90.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage91.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage92.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage93.pdf" target="_blank">Gavel-date?-static-one</a></p>
+            <p><a href="/static/clickerpage94.pdf" target="_blank">Gavel-date?-static-one</a></p>          `;
     }
 }
