634e23073f470410837114b63586ffdeb6a81cbd
dblaning <dblaning@alum.mit.edu>
2021-07-30 16:02:16 -0700

    Update Settings.js

--- frontend/static/js/views/Settings.js
+++ frontend/static/js/views/Settings.js
@@ -12,19 +12,19 @@ export default class extends AbstractView {
             <h2>Marshall High School Choir-Record Album</h2>
             <p>Album was produced and cut in 1970 or 1969</p>
             <p><a href="/static/01XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track01</a></p>
-            <p><a href="/static/02XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track02</a></p>
-            <p><a href="/static/03XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track03</a></p>
-            <p><a href="/static/04XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track04</a></p>
-            <p><a href="/static/05XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track05</a></p>
-            <p><a href="/static/06XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track06</a></p>
-            <p><a href="/static/07XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track07</a></p>
-            <p><a href="/static/08XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track08</a></p>
-            <p><a href="/static/09XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track09</a></p>
-            <p><a href="/static/10XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track10</a></p>
-            <p><a href="/static/11XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track11</a></p>
-            <p><a href="/static/12XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track12</a></p>
-            <p><a href="/static/13XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track13</a></p>
-            <p><a href="/static/14XV-01%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track14</a></p>
+            <p><a href="/static/01XV-02%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track02</a></p>
+            <p><a href="/static/01XV-03%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track03</a></p>
+            <p><a href="/static/01XV-04%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track04</a></p>
+            <p><a href="/static/01XV-05%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track05</a></p>
+            <p><a href="/static/01XV-06%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track06</a></p>
+            <p><a href="/static/01XV-07%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track07</a></p>
+            <p><a href="/static/01XV-08%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track08</a></p>
+            <p><a href="/static/01XV-09%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track09</a></p>
+            <p><a href="/static/01XV-10%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track10</a></p>
+            <p><a href="/static/01XV-11%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track11</a></p>
+            <p><a href="/static/01XV-12%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track12</a></p>
+            <p><a href="/static/01XV-13%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track13</a></p>
+            <p><a href="/static/01XV-14%20-%20Here%20We%20Come%20A%20Caroling.mp3" target="_blank">Track14</a></p>
             <p><a href="/static/IMG_2137.jpg" target="_blank">AlbumCover01</a></p>
             <p><a href="/static/IMG_2138.jpg" target="_blank">AlbumCover02</a></p>
             <p><a href="/static/IMG_2139.jpg" target="_blank">AlbumCover03</a></p>
