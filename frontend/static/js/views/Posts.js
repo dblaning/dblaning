9c822bb436dc381b19303d603ce3ad076df9c8f0
dblaning <dblaning@alum.mit.edu>
2021-08-20 13:19:29 -0700

    Update Posts.js

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -25,7 +25,7 @@ export default class extends AbstractView {
 
 </head>
 
-<body onload="MM_preloadImages(&#39;/static/menu1_1.gif&#39;)">
+<body onload="MM_preloadImages(&#39;/static/menu1_1.gif&#39;)" bgcolor="grey">
 <p>&nbsp;</p>
 <table align="center" cellpadding="0" cellspacing="0">
 <tbody>
