66368d062222d24684520bc9c0cc8760739c242d
dblaning <dblaning@alum.mit.edu>
2021-08-20 14:25:22 -0700

    Update Posts.js

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -84,7 +84,7 @@ export default class extends AbstractView {
 </tbody>
 </table>
 </td>
-<td class="black_line" style="width: 457px; height: 566px;">
+<td class="black_line" style="width: 457px; height: 616px;">
 <table class="list" cellpadding="0" cellspacing="0">
 <tbody>
 <tr>
