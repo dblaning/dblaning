cb6c533dcd2f75ce122f5ed5be3e6bb86fdb3c78
dblaning <dblaning@alum.mit.edu>
2021-08-20 10:42:32 -0700

    Update Dashboard.js

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -8,20 +8,7 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>DavidLaning.com - Home</h1>
-            <p>
-              This website is under development.  Please enjoy reading the Marshall High School Gavels while this site is being developed.<br>
-              Best regards<br>
-              Dave Laning<br>
-              <a href="/static/2021-08-06_13-19-56-DavidLaningProfileCropped.png" target="_blank" width="797" height="4366" >LinkedIn Profile</a><br>
-              
-            </p>
-            <p>
-                <a href="/posts" data-link>View Marshall Gavels</a>.
-            </p>
-                        <p>
-                <a href="/settings" data-link>Listen to Marshall's Choir</a>.
-            </p>
+            <h1>DavidLaning.com</h1>
             <p><iframe src="/static/2021-08-06_13-19-56-DavidLaningProfileCropped.png" width="800" height="600"></iframe></p>
         `;
     }
